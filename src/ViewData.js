8b7d0d9919b8afd59a5596d75d73c70d7b5d4b70
Cameron McGinley <49362691+cameronmcginley@users.noreply.github.com>
2022-02-20 23:36:07 -0600

    Fixed signin delete button

--- src/ViewData.js
+++ src/ViewData.js
@@ -2,13 +2,13 @@ import React, { Component, useState, useEffect } from "react";
 import "./ViewData.css";
 import { db } from "./firebase-config";
 import {
-  collection,
-  getDocs,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  doc,
-  FieldValue,
+    collection,
+    getDocs,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+    FieldValue,
 } from "firebase/firestore";
 
 const ViewData = () => {
@@ -21,52 +21,59 @@ const ViewData = () => {
     };
 
     const deleteSignin = async (id) => {
-        const courseDoc = doc(db, "courses", id);
-        await deleteDoc(courseDoc);
+        const signinDoc = doc(db, "sign-ins", id);
+        await deleteDoc(signinDoc);
         getSigninData();
     };
-    
+
     // called for rendering
     useEffect(() => {
         getSigninData();
     }, []);
-    
+
     return (
         <div className="App">
             <table>
-            <tr>
-                <th>First Name</th>
-                <th>Course ID</th>
-                <th>Time Logged</th>
-            </tr>
-
-            {/* Adds each course as a row in the table */}
-            {signinData.map((signin) => {
-                // toDate() errors if there is no time saved
-                const hasTime = Boolean(signin.currentTime)
-
-                return (
                 <tr>
-                    <th>{signin.userFirstName}</th>
-                    <th>{signin.courseID}</th>
-                    <th>{hasTime ? (signin.currentTime.toDate().toDateString() + " " 
-                    + signin.currentTime.toDate().toLocaleTimeString('en-US') ) : ("")}</th>
-                    <button class="deletebtn"
-                    onClick={() => {
-                        deleteSignin(signin.id);
-                    }}
-                    >
-                    Delete
-                    </button>
+                    <th>First Name</th>
+                    <th>Course ID</th>
+                    <th>Time Logged</th>
                 </tr>
-                );
-            })}
+
+                {/* Adds each course as a row in the table */}
+                {signinData.map((signin) => {
+                    // toDate() errors if there is no time saved
+                    const hasTime = Boolean(signin.currentTime);
+
+                    return (
+                        <tr>
+                            <th>{signin.userFirstName}</th>
+                            <th>{signin.courseID}</th>
+                            <th>
+                                {hasTime
+                                    ? signin.currentTime
+                                          .toDate()
+                                          .toDateString() +
+                                      " " +
+                                      signin.currentTime
+                                          .toDate()
+                                          .toLocaleTimeString("en-US")
+                                    : ""}
+                            </th>
+                            <button
+                                class="deletebtn"
+                                onClick={() => {
+                                    deleteSignin(signin.id);
+                                }}
+                            >
+                                Delete
+                            </button>
+                        </tr>
+                    );
+                })}
             </table>
-
-        </div>  
+        </div>
     );
-}
-
-
+};
 
 export default ViewData;
