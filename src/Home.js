789f0ec2f9c0d5908ca0cbf4edb0ba10c9713430
Dean Lang <98561976+KonoDeanoDa@users.noreply.github.com>
2022-03-07 15:52:44 -0600

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase-config";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate()
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+  
+  if (!user) {
+	  navigate('/login')
+  }
+  
   return (
     <div className="home">
         <h1>Hello</h1>
