115b632a2b6e881fe27d163d5817db1fc36ae08b
Cameron McGinley <49362691+cameronmcginley@users.noreply.github.com>
2022-03-10 18:00:05 -0600

    Added CSV export back to view data page

--- src/Pages/ViewData.js
+++ src/Pages/ViewData.js
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
 import FirebaseDataTable from "../Components/FirebaseDataTable";
+import AlertDialog from "../Components/AlertDialog";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -22,6 +23,7 @@ const Home = () => {
       <h1>Hello</h1>
 
       {/* CSV Export */}
+      <AlertDialog />
 
       {/* Table with User Signin Data */}
       {/* Headers must be defined in src/Functions/FirebaseDataTable/TableHeaders.js */}
