36de85cdb80984ff4fe61584a8b580221f64e666
Dean Lang <98561976+KonoDeanoDa@users.noreply.github.com>
2022-04-25 23:08:22 -0500

    Implemented MUI and got rid of HTML

--- src/Pages/register.js
+++ src/Pages/register.js
@@ -2,22 +2,24 @@ import { useState } from "react";
 import { createUserWithEmailAndPassword, sendEmailVerification, signOut } from "firebase/auth";
 import { auth } from "../firebase-config";
 import { Link, useNavigate } from "react-router-dom";
+import { TextField, Button, Box, Container, Grid } from "@mui/material";
 
 function Register() {
   const navigate = useNavigate();
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [user, setUser] = useState({});
+  const [errMessage, setErrMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
   const [passwordShown, setPasswordShown] = useState(false);
-
-  const errMessage = document.getElementById("errorMessage");
 
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
   };
-
+  
   const register = async () => {
-	  errMessage.innerHTML = "";
+	  setErrMessage("");
+	  setSuccessMessage("");
 	  
 	  if (registerEmail.endsWith('gmail.com'))
 	  {
@@ -27,66 +29,84 @@ function Register() {
 	  		registerPassword
 	  	).then().catch(error => {
 		  	switch (error.code) {
-				case 'auth/email-already-in-use':
-					errMessage.innerHTML = "User with given e-mail address already exists.";
-					errMessage.style.color = "red";
-           				break;
+		  		case 'auth/email-already-in-use':
+					setErrMessage("User with given e-mail address already exists.");
+           			break;
 			  	case 'auth/invalid-email':
-            				errMessage.innerHTML = "E-mail address invalid.";
-					errMessage.style.color = "red";
+            		setErrMessage("E-mail address invalid.");
 					break;
 				default:
 		  	}
 	  	});
   	}
+	else
+	{
+		setErrMessage("E-mail address must be @wichita.gov");
+	}
 		
 	if (auth.currentUser)
 	{
-		if (!registerEmail.endsWith('gmail.com'))
-		{
-			errMessage.style.color = "red";
-			errMessage.innerHTML = "E-mail address must be @wichita.gov";
-		}
-		else
-		{
-			sendEmailVerification(auth.currentUser);
-			errMessage.innerHTML = "Verification e-mail has been sent. Verify and then sign-out above. Then use the login page to sign in to redirect to the home page.";
-			errMessage.style.color = "green";
-			signOut(auth);
-		}
+		sendEmailVerification(auth.currentUser);
+		setSuccessMessage("Verification e-mail has been sent. Verify and then sign-out above. Then use the login page to sign in to redirect to the home page.");
+		signOut(auth);
 	}
   };
 
   return (
-    <div className="App">
-      <div>
-        <h3>Create Account</h3>
-        <input
-          placeholder="E-Mail"
-          onChange={(event) => {
-            setRegisterEmail(event.target.value);
-          }}
-        />
-        <p></p>
-        <input
-          type={passwordShown ? "text" : "password"}
-          placeholder="Password"
-          onChange={(event) => {
-            setRegisterPassword(event.target.value);
-          }}
-        />
-        <p></p>
-        <button onClick={togglePassword}>Show Password</button>
-        <p></p>
-        <button onClick={register}>Create Account</button>
-        <p></p>
-        <div id="errorMessage"></div>
-        <p></p>
-        <div>
-          Already have an account? <Link to="/login">Login</Link> now.
-        </div>
-      </div>
-    </div>
+	  <>
+	<Container className="App" maxWidth="xs">
+		<Box
+		sx={{
+			paddingTop: 4,
+			paddingBottom: 4,
+		}}
+		>
+			<p style={{ color: "red" }}>{errMessage}</p>
+			<p style={{ color: "green" }}>{successMessage}</p>
+			
+			<Box sx={{ mt: 1 }}>
+				<h1>Create Account</h1>
+
+				<TextField
+				margin="normal"
+				required
+				fullWidth
+				label="Email Address"
+				autoFocus
+				onInput={(e) => setRegisterEmail(e.target.value)}
+				/>
+
+				<TextField
+				margin="normal"
+				required
+				fullWidth
+				label="Password"
+				type={passwordShown ? "text" : "password"}
+				onInput={(e) => setRegisterPassword(e.target.value)}
+				/>
+
+				<Button onClick={togglePassword}>Show Password</Button>
+
+				<Button
+				onClick={register}
+				fullWidth
+				variant="contained"
+				sx={{ mt: 3, mb: 2 }}
+				>
+				Create Account
+				</Button>
+
+				<Grid container>
+				<Grid item xs>
+					<Link to="/login" variant="body2">
+					{"Login Here"}
+					</Link>
+				</Grid>
+				</Grid>
+			</Box>
+		</Box>
+	</Container>
+	</>
   );
 }
 
